bb4f5635f2ed0ffb3d7838189a0ffb90b79144fe
Komal Yadav <komalyadav@192.168.1.3>
2024-01-23 01:18:21 +0530

    login signup added

---
+++ BookStoreFrontend/src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private isAuthenticated = false;
+  isLoggedIn(): boolean {
+    return this.isAuthenticated;
+  }
+
+  
+  private apiUrl='http://localhost:3000'
+  constructor(private http : HttpClient) { }
+
+  registerUser(user: User): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/users`, user);
+  }
+
+  loginUser(email:string): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users?email=${email}`);
+  }
+
+  
+}
+
+
