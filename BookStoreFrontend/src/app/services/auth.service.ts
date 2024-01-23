ca1f7b0bf08f985a912a4706fd544682dd68b420
Komal Yadav <komalyadav@192.168.1.3>
2024-01-23 19:36:05 +0530

    nav bar updated

--- BookStoreFrontend/src/app/services/auth.service.ts
+++ BookStoreFrontend/src/app/services/auth.service.ts
@@ -2,28 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/auth';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticated = false;
-  isLoggedIn(): boolean {
-    return this.isAuthenticated;
-  }
 
+  isLoggedIn: boolean = false;
   
   private apiUrl='http://localhost:3000'
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient,
+    private router :Router ) { }
 
   registerUser(user: User): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/users`, user);
   }
 
   loginUser(email:string): Observable<User[]> {
+    this.isLoggedIn=true;
     return this.http.get<User[]>(`${this.apiUrl}/users?email=${email}`);
   }
 
+  LogoutUser(){
+    this.isLoggedIn=false;
+    sessionStorage.clear();
+    this.router.navigate(['home']);
+  }
   
 }
 
